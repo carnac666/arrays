762fce1f5090f0d775a76cae4556bec0c67fde69
Carles Castillejo <carlescn@openmailbox.org>
2016-04-21 12:48:55 +0200

    PRIMER CANVI

--- printArrays.java
+++ printArrays.java
@@ -1,7 +1,10 @@
 public class printArrays{
 
   public static void main (String [] args) {
-  
+ 
+ 	
+	int[][] arrayBi ={{5,3,2,3},{3,9,8,9},{5,7,12,7}};
+
   System.out.println (" Hola caracola");
   }
 
